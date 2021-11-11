5daf5882ef4258d11b94d3ea3aec0b72f04c11b8
AndriyanTW <83396281+AndriyanTW@users.noreply.github.com>
2021-11-11 13:41:28 +0700

    Add files via upload

---
+++ tugas10.py
@@ -0,0 +1,25 @@
+from direct.showbase.ShowBase import ShowBase
+
+class Myapp(ShowBase):
+    def __init__(self):
+        super().__init__()
+        # Load the environment model.
+        self.scene = self.loader.loadModel("models/panda")
+        # Reparent the model to render.
+        self.scene.reparentTo(self.render)
+       # Apply scale and position transforms on the model.
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+        self.panda = self.loader.load_model("panda.egg.pz")
+        self.panda.reparentTo(self.render)
+        self.panda.setScale(2, 2, 2)
+        self.panda.setPos(-8, 42, 0)
+        self.music = self.loader.loadSfx("assets/yooooooooooooooooooooooooo_4.mp3")
+        self.music.setLoop(True)
+        self.music.play()
+        self.music.setVolume(0.9)
+
+
+app = Myapp()
+app.run()
